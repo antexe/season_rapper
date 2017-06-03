cb4b482620c07de169a9bb1bc90c883b810276a4
shoichiyamazaki <shoichiyamazaki@shoichiyamazakinoMacBook-Pro.local>
2017-06-03 19:07:07 +0900

    SpeechAPIをBattleVCへ実装

--- season_rapper/BattleViewController.swift
+++ season_rapper/BattleViewController.swift
@@ -7,12 +7,124 @@
 //
 
 import UIKit
+import Speech
 
-class BattleViewController: UIViewController {
+class BattleViewController: UIViewController,SFSpeechRecognizerDelegate {
     
     @IBOutlet weak var inputLabel: UILabel!
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    private let audioEngine = AVAudioEngine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        speechRecognizer.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            /*
+             The callback may not be called on the main thread. Add an
+             operation to the main queue to update the record button's state.
+             */
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                        break
+                case .denied:
+                    break
+                case .restricted:
+                    break
+                case .notDetermined:
+                    break
+                }
+            }
+        }
+        
+        try! startRecording()
+    }
+    
+    
+    private func startRecording() throws {
+        
+        // Cancel the previous task if it's running.
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else { fatalError("Audio engine has no input node") }
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        // Configure request so that results are returned before audio recording is finished
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // A recognition task represents a speech recognition session.
+        // We keep a reference to the task so that it can be cancelled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if let result = result {
+                
+                print("recognitionReq:\(recognitionRequest)")
+                print("result:\(result)")
+                print("hypothesis:\(result.transcriptions)")
+                print("besttranscription:\(result.bestTranscription)")
+                print("formatedStr:\(result.bestTranscription.formattedString)")
+                
+                
+                let list = result.bestTranscription.segments.flatMap{$0}
+                let list01 = result.transcriptions.flatMap{$0}
+                
+                
+                for item in list{
+                    print("01:\(item)")
+                    item.substring
+                    print("01-1:\(item.alternativeSubstrings)")
+                    
+                    
+                }
+                
+                for item in list01{
+                    print("02:\(item)")
+                    print("02-1:\(item.formattedString)")
+                }
+                
+                self.inputLabel.text = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+            
+        }
+        
+        audioEngine.prepare()
+        
+        try audioEngine.start()
         
     }
 }
