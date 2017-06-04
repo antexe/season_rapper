0218fcc8cf7cc77fe785bfe585579f9b67dc7636
藤川慶 <fujikawakei@fujikawakei-no-MacBook.local>
2017-06-04 11:01:33 +0900

    最終調整。

--- season_rapper/ResultViewController.swift
+++ season_rapper/ResultViewController.swift
@@ -18,20 +18,11 @@ class ResultViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            ScreenTransitionManager.shared.goToLast()
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
